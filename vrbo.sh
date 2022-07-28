ff368228e347cf425da1e1d26fc49a1b3af11d3b
Glyn Gibson <glyn.gibson@gmail.com>
2022-07-28 16:10:30 -0700

    WIP
    - Add fzf + vim support
    - Rename helpers

--- vrbo.sh
+++ vrbo.sh
@@ -3,36 +3,49 @@
 # No Ansible For Old Men
 #
 
-export VRBO_ROOT=/dev/shm/glyn-home
+export VRBO=/dev/shm/glyn-home
+export VRBO_TMP=/tmp/vrbo
 
-cfg_fzf() {
+vrbo_cfg_fzf() {
+    echo Setup fzf
+    git clone --depth 1 https://github.com/junegunn/fzf.git $VRBO/.fzf
+    $VRBO/.fzf/install
+}
+
+vrbo_cfg_vim() {
+    echo Setup vim
+    mkdir -p $VRBO/.vim
+    cp vimrc $VRBO/.vim/.vimrc
+    ln -s $VRBO/.vim/.vimrc $VRBO/.vimrc
+}
+
+vrbo_cfg_tmux() {
     echo wat
 }
 
-cfg_vim() {
-    echo wat
-}
-
-cfg_tmux() {
-    echo wat
-}
-
-cfg_env() {
-    echo wat
+vrbo_cfg_env() {
+    echo Configuing VRBO environment
     mkdir -p $VRBO/{bin,etc,var,run,tmp}
+    mkdir -p $VRBO_TMP
 
     export PATH=$PATH:$VRBO/bin
+
+    echo 'export PATH=$PATH:$VRBO/bin' >> $VRBO/.bashrc
 }
 
-cfg_scripts() {
+vrbo_cfg_scripts() {
     echo "Copy a subset from private ~/etc repo"
     echo "Hmm should I make this a subrepo of ~/etc/? and then just symlink?"
 }
 
 
-main() {
+vrbo_main() {
     cfg_env
     cfg_fzf
+}
+
+vrbo_sh() {
+    HOME=$VRBO /bin/bash
 }
 
 
