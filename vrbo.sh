9978dbdb17676c09e8136fe99fd2732273fa8dfb
Netskrt Systems Inc <netskrt@node8.dev.netskrt.org>
2022-07-28 17:08:43 -0700

    Add fn to import existing stuff to $VRBO/
    I wanna run Helm n stuff. Also stash old $HOME before starting a shell

--- vrbo.sh
+++ vrbo.sh
@@ -6,6 +6,8 @@
 export VRBO=/dev/shm/glyn-home
 export VRBO_TMP=/tmp/vrbo
 
+export OLDHOME=/home/netskrt
+
 vrbo_cfg_fzf() {
     echo Setup fzf
     git clone --depth 1 https://github.com/junegunn/fzf.git $VRBO/.fzf
@@ -38,6 +40,15 @@ vrbo_cfg_scripts() {
     echo "Hmm should I make this a subrepo of ~/etc/? and then just symlink?"
 }
 
+vrbo_import() {
+    # NB: can't run this if you've switch path already
+    echo "Importing settings from existing user"
+
+    cp -r ~/.kube $VRBO
+    cp -r ~/.aws $VRBO
+    cp -r ~/.docker $VRBO
+}
+
 
 vrbo_main() {
     cfg_env
@@ -45,6 +56,7 @@ vrbo_main() {
 }
 
 vrbo_sh() {
+    export OLD_HOME=$HOME
     HOME=$VRBO /bin/bash
 }
 
